3cbc0028c5d7be0318304c63bcfb7194562a7cc3
TreeBomb-01 <150654998+TreeBomb-01@users.noreply.github.com>
2025-11-18 00:55:58 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8408 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,17 @@
+import sys
+#어렵다 ㅈㅈ
+
+input = sys.stdin.readline
+
+N = int(input())
+
+count = [0] * 10001
+
+for _ in range(N):
+    num = int(input())
+    count[num] += 1
+
+for i in range(10001):
+    if count[i] != 0:
+        for j in range(count[i]):
+            print(i)
