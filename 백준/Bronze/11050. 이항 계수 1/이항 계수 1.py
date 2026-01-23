94c5af507221202351ad475de7c995f58e4c74d1
TreeBomb-01 <150654998+TreeBomb-01@users.noreply.github.com>
2026-01-23 18:05:31 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,4 @@
-def factorial_for(n):
+def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
@@ -8,8 +8,8 @@ n, k = map(int,input().split())
 if k == 0:
     print(1)
 else:
-    nf = factorial_for(n)
-    kf = factorial_for(k)
-    nkf = factorial_for(n-k)
+    nf = factorial(n)
+    kf = factorial(k)
+    nkf = factorial(n-k)
 
     print(int(nf/(kf*nkf)))
